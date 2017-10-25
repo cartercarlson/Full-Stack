9e7698f33a2d7d98fdf9b905e5238c6c709ccf0e
Carter Carlson <carterbud1@gmail.com>
2017-10-25 14:02:59 -0500

    Remove file namer from folder

--- Programming Fundamentals and the Web/Prank code/rename_files.py
+++
@@ -1,5 +0,0 @@
-import os
-def rename_files():
-	file_list = os.listdir(r"C:/Users/Carter Carlson/Documents/Udacity/Full Stack Nanodegree/Programming Fundamentals and the Web/Prank code")
-	print file_list
-rename_files()
