cd07fbc8641f3e6d63769b02d43cddb9998157b8
Carter Carlson <carterbud1@gmail.com>
2017-10-25 14:12:40 -0500

    Find prank solution

--- Programming Fundamentals and the Web/rename_files.py
+++ Programming Fundamentals and the Web/rename_files.py
@@ -1,5 +1,8 @@
 import os
 def rename_files():
 	file_list = os.listdir(r"C:/Users/Carter Carlson/Documents/Udacity/Full Stack Nanodegree/Programming Fundamentals and the Web/Prank code")
-	print file_list
+
+	os.chdir(r"C:/Users/Carter Carlson/Documents/Udacity/Full Stack Nanodegree/Programming Fundamentals and the Web/Prank code")
+	for file_name in file_list:
+		os.rename(file_name, file_name.translate(None, "0123456789"))
 rename_files()
