75628f4fb1fb7dc0233d4a786c88439bc6d98931
Carter Carlson <carterbud1@gmail.com>
2017-10-26 08:34:38 -0500

    Start movie making .py code

---
+++ Programming Fundamentals and the Web/entertainmen_center.py
@@ -0,0 +1,12 @@
+import media
+
+rambo = media.Movie("Toy Story", "A movie of Sylvester Stallone kicking ass",
+	"http://egmnowbeta.s3-us-west-2.amazonaws.com/wp-content/uploads/2016/11/03074722/john-rambo-bye-838x471.jpg",
+	"https://www.youtube.com/watch?v=J5SdTeijDhY")
+
+print(rambo.storyline)
+
+nemo = media.Movie("Finding Nemo","Little Nemo gets lost in the big blue ocean",
+	"https://larryfire.files.wordpress.com/2011/10/pva6ds.jpg",
+	
+	)
