46098943c9f947859684ebf53eded28149631d15
Carter Carlson <carterbud1@gmail.com>
2017-10-26 12:36:43 -0500

    Revised entertainment_center.py and media.py to resubmit movie project

--- Programming Fundamentals and the Web/entertainment_center.py
+++ Programming Fundamentals and the Web/entertainment_center.py
@@ -1,19 +1,29 @@
 import media
 import fresh_tomatoes
 
-rambo = media.Movie("Rambo", "A movie of Sylvester Stallone kicking ass",
-	"https://images-na.ssl-images-amazon.com/images/I/51VCKIqNU2L.jpg",
-	"https://www.youtube.com/watch?v=J5SdTeijDhY")
+# Rambo movie: movie title, storyline, poster image and movie trailer
+rambo = media.Movie(
+    "Rambo",
+    "A movie of Sylvester Stallone kicking ass",
+    "https://images-na.ssl-images-amazon.com/images/I/51VCKIqNU2L.jpg",
+    "https://www.youtube.com/watch?v=J5SdTeijDhY")
 
-nemo = media.Movie("Finding Nemo","Little Nemo gets lost in the big blue ocean",
-	"https://larryfire.files.wordpress.com/2011/10/pva6ds.jpg",
-	"https://www.youtube.com/watch?v=ndq9-vmD058")
+# Finding Nemo movie: movie title, storyline, poster image and movie trailer
+nemo = media.Movie(
+    "Finding Nemo",
+    "Little Nemo gets lost in the big blue ocean",
+    "https://larryfire.files.wordpress.com/2011/10/pva6ds.jpg",
+    "https://www.youtube.com/watch?v=ndq9-vmD058")
 
-dory = media.Movie("Finding Dory", "The knockoff of Finding Nemo",
-	"https://storemedia.cineplex.com/store/promo/posters/SuperTicket_Poster_800x1200_FindingDoryfinal.jpg",
-	"https://www.youtube.com/watch?v=3JNLwlcPBPI")
+# Finding Dory movie: movie title, storyline, poster image and movie trailer
+dory = media.Movie(
+    "Finding Dory",
+    "The knockoff of Finding Nemo",
+    "https://storemedia.cineplex.com/store/promo/posters/SuperTicket_Poster_800x1200_FindingDoryfinal.jpg",  # NOQA
+    "https://www.youtube.com/watch?v=3JNLwlcPBPI")
 
+# Set the movies that will go to the media file
 movies = [rambo, nemo, dory]
+
+# Open the HTML file in a webbrowser using fresh_tomatoes.py
 fresh_tomatoes.open_movies_page(movies)
-#print media.Movie.VALID_RATINGS
-#print media.Movie.__doc__, media.Movie.__name__, media.Movie.__module__
