6c98fc1f0a4c6f1562678aa435a5ec4fc4e90574
Carter Carlson <carterbud1@gmail.com>
2017-10-26 11:00:38 -0500

    Add README.txt for project submission

--- Programming Fundamentals and the Web/media.py
+++ Programming Fundamentals and the Web/media.py
@@ -10,7 +10,7 @@ class Movie():
 		self.storyline = movie_storyline
 		self.poster_image_url = poster_image
 		self.trailer_youtube_url = trailer_youtube
-	# self is the object being created- points to rambo..
+	# self is the object being created
 
 	def show_trailer(self):
 		webbrowser.open(self.trailer_youtube_url)
