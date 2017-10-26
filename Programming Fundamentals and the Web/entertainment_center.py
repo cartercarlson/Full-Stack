2f8f8756a6fa8b184ae6568f8f08d52d27898321
Carter Carlson <carterbud1@gmail.com>
2017-10-26 09:27:25 -0500

    Edit entertainment and meida .py

--- Programming Fundamentals and the Web/entertainment_center.py
+++ Programming Fundamentals and the Web/entertainment_center.py
@@ -1,12 +1,23 @@
 import media
+import fresh_tomatoes
 
-rambo = media.Movie("Toy Story", "A movie of Sylvester Stallone kicking ass",
-	"http://egmnowbeta.s3-us-west-2.amazonaws.com/wp-content/uploads/2016/11/03074722/john-rambo-bye-838x471.jpg",
+rambo = media.Movie("Rambo", "A movie of Sylvester Stallone kicking ass",
+	"https://images-na.ssl-images-amazon.com/images/I/51VCKIqNU2L.jpg",
 	"https://www.youtube.com/watch?v=J5SdTeijDhY")
-
-print(rambo.storyline)
+#print(rambo.storyline)
 
 nemo = media.Movie("Finding Nemo","Little Nemo gets lost in the big blue ocean",
 	"https://larryfire.files.wordpress.com/2011/10/pva6ds.jpg",
-	
-	)
+	"https://www.youtube.com/watch?v=ndq9-vmD058")
+#print nemo.storyline
+#nemo.show_trailer()
+
+dory = media.Movie("Finding Dory", "The knockoff of Finding Nemo",
+	"https://storemedia.cineplex.com/store/promo/posters/SuperTicket_Poster_800x1200_FindingDoryfinal.jpg",
+	"https://www.youtube.com/watch?v=3JNLwlcPBPI")
+
+movies = [rambo, nemo, dory]
+#fresh_tomatoes.open_movies_page(movies)
+#print media.Movie.VALID_RATINGS
+
+print media.Movie.__doc__, media.Movie.__name__, media.Movie.__module__
