4f6b5d503f230c44f9e9edd57a23910068ced203
Carter Carlson <carterbud1@gmail.com>
2017-11-15 09:42:04 -0600

    Project 3 Final

--- Project 3- Log Analysis/README.md
+++ Project 3- Log Analysis/README.md
@@ -1,19 +1,34 @@
-### Getting Started
-To use this project you need to have python and newsdatadb.sql installed
-
-You also need to install MySQLdb to your computer:
-https://stackoverflow.com/questions/372885/how-do-i-connect-to-a-mysql-database-in-python
-
----
-
-How to run the code
-1. Download attached python script
-2. Run newsdatadb.py
-3. Enjoy!
+# Log Analysis
+## Getting Started
+To use this project you need to have python and PostgreSQL installed.
+In order for the python code to run, you need a Python interpreter, the
+database, and the corresponding Python library containing the APIs to 
+interact with the database software.	
 
 ---
 
-Files included
+### Introduction
+The purpose of this project was to get practice interacting from a live
+database both from the command line and from my code.  I then built and
+refined complex queries and used them to draw conclusions.  The database
+represents a log of a fictitious newspaper website, and the three questions
+From my analysis I answer these questions:
+1. What are the top three articles of all time?
+2. Who are the most popular article authors of all time?
+3. On which days did more than 1 percent of requests lead to errors?
+---
+
+### How to run the code
+1. Download attached python script and zip file containing the SQL script.
+2. If you use the provided Vagrantfile from Udacity, it will create a database
+named news.  If you don't use Vagrant, you will need to create the **news**
+database.
+**3. Use this command to connect to the database: psql -d news -f newsdata.sq
+and execute**
+
+---
+
+#### Files included
 newsdatadb.py
 This code is the python script used to run the SQL queries on the database to
 answer the 3 questions provided.
@@ -24,14 +39,38 @@ in the Vagrant VM, copied and pasted.
 
 ---
 
-Views Created
+#### Views Created
 
-numerator
-This view counts the total number of errors for each day.  I chose denominator
-because it's easy to use when it actually is used as a denominator.
+#### query2_view
+CREATE VIEW query2_view AS
+SELECT articles.author AS name, count(log.path) AS views
+FROM articles, log
+WHERE articles.slug=substring(path, 10)
+GROUP BY articles.author
+ORDER BY views DESC;
 
-denominator
+This view groups together the total count of views for each author.  This 
+records articles.author which is the author id number, so from here I 
+find the names based on id numbers.
+
+##### numerator
+
+CREATE VIEW numerator AS
+SELECT time::date AS date_group, count(*) AS count
+FROM log
+WHERE status != '200 OK'
+GROUP BY date_group;
+
+This view counts the total number of errors for each day.  I chose numerator
+because it's easy to use when it actually is used as a numerator.
+
+###### denominator
+
+CREATE VIEW denominator AS
+SELECT time::date AS date_group, count(*) AS count
+FROM log
+GROUP BY date_group;
+
 This view counts the total number of status updates.  I think you can guess
 why it's named denominator...
-
 Enjoy! :)
--- Project 3- Log Analysis/newsdatadb.py
+++ Project 3- Log Analysis/newsdatadb.py
@@ -1,93 +1,62 @@
+#!/usr/bin/env python
 # "Database code" for newsdata.
 # Python 2.7.13
-import MySQLdb
+import psycopg2
 
-DBNAME = "newstatadb"
-db = MySQLdb.connect(database=DBNAME)
-c = db.cursor()
-# change spaces to dashes
-c.execute("""UPDATE articles
-    SET title = REPLACE(title, ' ', '-')
-    """)
-# remove everything after comma in title
-c.execute("""UPDATE articles
-    SET title = left(title, strpos(title, ',') -1)
-    """)
-# remove apostrophe
-c.execute("""UPDATE articles
-    SET title = REPLACE(title, '''', '')
-    """)
-# Shorten article.title lengths so they are as short as log.paths.
-# I found that if I didn't shorten a few of the article titles, the
-# update would skip over the log.path and not change the log.path to
-# the correct title
-c.execute("""UPDATE articles
-    SET title = REPLACE(title,
-    'There-are-a-lot-of-bear', 'So-many-bears')
-    """)
-c.execute("""UPDATE articles
-    SET title = REPLACE(title,
-    'Goats-eat-Googles-law', 'Goats-eat-Googles')
-    """)
-c.execute("""UPDATE articles
-    SET title = REPLACE(title,
-    'Trouble-for-troubled-troublemaker', 'Trouble-for-troubled')
-    """)
-# Now that all the article.titles work, fix log.path records to the correct
-# matching title
-c.execute("""UPDATE log
-    SET path = articles.title
-    FROM articles where log.path ~* articles.title
-    """)
+def get_query_results(query):
+    DBNAME = "news"
+    db = psycopg2.connect(database=DBNAME)
+    c = db.cursor()
+    c.execute(query)
+    result = c.fetchall()
+    db.close()
+    return result
 
-# Query for Question 1: What are the most popular 3 articles of all time?
-c.execute("""SELECT articles.title AS title, count(log.path) AS views
-    FROM articles
-    LEFT JOIN log ON title = log.path
-    GROUP BY title
-    ORDER BY views DESC
-    LIMIT 3
-    """)
-c.fetchall()
-db.commit()
-db.close()
 
-# Query for Question 2: Who are the most popular article authors of all time?
-db = MySQLdb.connect(database=DBNAME)
-c = db.cursor()
-c.execute("""SELECT authors.name AS name, count(log.path) AS views
-    FROM articles
-    LEFT JOIN log ON articles.title = log.path
-    LEFT JOIN authors ON articles.author = authors.id
-    GROUP BY name
-    ORDER BY views DESC
-    """)
-c.fetchall()
-db.close()
-
-# Queries for Question 3: On which days did more than 1% of requests lead to
-# errors?
-db = MySQLdb.connect(database=DBNAME)
-c = db.cursor()
-# Create view of the count of errors per day
-c.execute("""CREATE VIEW numerator AS
-    SELECT time::date AS date_group, count(*) AS count
+def print_top_articles():
+    """Prints out the top 3 articles of all time."""
+    query = """SELECT title, count(*) AS views
     FROM log
-    WHERE status != '200 OK'
-    GROUP BY date_group
-    """)
-# Create view of the count of all status updates
-c.execute("""CREATE VIEW denominator AS
-    SELECT time::date AS date_group, count(*) AS count
-    FROM log
-    GROUP BY date_group
-    """)
-# Actual query
-c.execute("""SELECT numerator.date_group,
-    (numerator.count * 100.0 / denominator.count) AS percent
+    JOIN articles ON log.path = concat('/article/', articles.slug)
+    GROUP BY articles.title, status
+    ORDER BY views DESC limit 3"""
+    results = get_query_results(query)
+
+    print("\nWhat are the top three articles of all time?\n")
+    for title, views in results:
+        print("  {} -- {} views".format(title, views))
+
+
+def print_top_authors():
+    """Prints a list of authors ranked by article views."""
+    query = """
+    SELECT authors.name, views
+    FROM authors, query2_view
+    WHERE authors.id = query2_view.name"""
+    results = get_query_results(query)
+
+    print("\nWho are the most popular article authors of all time?\n")
+    for author, views in results:
+        print("  {} -- {} views".format(author, views))
+
+
+def print_errors_over_one():
+    """Prints out the days where more than 1% of logged access requests were
+    errors."""
+    query = """SELECT numerator.date_group,
+    (ROUND(numerator.count * 100.0 / denominator.count, 2)) AS percent
     FROM numerator, denominator
     WHERE numerator.date_group = denominator.date_group AND
-        numerator.count * 100.0 / denominator.count > 1
-    """)
-c.fetchall()
-db.close()
+        numerator.count * 100.0 / denominator.count > 1"""
+    results = get_query_results(query)
+
+    print(
+        "\nWhich day did more than 1 percent of requests lead to errors?\n")
+    for day, views in results:
+        print("  {} -- {}%".format(day, views))
+
+
+if __name__ == '__main__':
+    print_top_articles()
+    print_top_authors()
+    print_errors_over_one()
