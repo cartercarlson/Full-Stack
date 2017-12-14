83fffdea1b0e025352bdd169c0ff3292cc47f48e
Carter Carlson <carterbud1@gmail.com>
2017-12-14 13:25:12 -0600

    Add locations for project in app.js

---
+++ Project 5- Neighborhood Map/js/app.js
@@ -0,0 +1,34 @@
+var model = {
+	points: [
+	{
+		title: "Childhood House",
+		location: {lat: 41.796384, lng: -88.417474},
+		iconColor: "#A93226"
+	},
+	{
+		title: "Raging Waves Waterpark",
+		location: {lat: 41.6967971, lng: -88.4176992},
+		iconColor: "#1F618D"
+	},
+	{
+		title: "Chicago Premium Outlets",
+		location: {lat: 41.7829187, lng: -88.3169258},
+		iconColor: "#1E8449" 
+	},
+	{
+		title: "Chipotle",
+		location: {lat: 41.7546809, lng: -88.3936599},
+		iconColor: "#BA4A00"
+	},
+	{
+		title: "Marmion Academy",
+		location: {lat: 41.8153066, lng: -88.2961499},
+		iconColor: "#839192"
+	},
+	{
+		title: "Phillips Park Zoo",
+		location: {lat: 41.7494086, lng: -88.3504218},
+		iconColor: "#6C3483"
+	}
+	]
+};
