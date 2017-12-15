dde4cddc567a0328f47418b537e37955e3d3a1c6
Carter Carlson <carterbud1@gmail.com>
2017-12-15 10:07:30 -0600

    Add sidebar with locations, move knockout lib to correct folder

--- Project 5- Neighborhood Map/js/app.js
+++ Project 5- Neighborhood Map/js/app.js
@@ -1,34 +1,60 @@
-var model = {
-	points: [
-	{
-		title: "Childhood House",
-		location: {lat: 41.796384, lng: -88.417474},
-		iconColor: "#A93226"
-	},
+var initialPoints = [
 	{
 		title: "Raging Waves Waterpark",
-		location: {lat: 41.6967971, lng: -88.4176992},
+		lat: 41.6967971,
+		lng: -88.4176992,
 		iconColor: "#1F618D"
 	},
 	{
 		title: "Chicago Premium Outlets",
-		location: {lat: 41.7829187, lng: -88.3169258},
+		lat: 41.7829187,
+		lng: -88.3169258,
 		iconColor: "#1E8449" 
 	},
 	{
 		title: "Chipotle",
-		location: {lat: 41.7546809, lng: -88.3936599},
+		lat: 41.7546809,
+		lng: -88.3936599,
 		iconColor: "#BA4A00"
 	},
 	{
 		title: "Marmion Academy",
-		location: {lat: 41.8153066, lng: -88.2961499},
-		iconColor: "#839192"
+		lat: 41.8153066,
+		lng: -88.2961499,
+		iconColor: "#A93226"
 	},
 	{
 		title: "Phillips Park Zoo",
-		location: {lat: 41.7494086, lng: -88.3504218},
+		lat: 41.7494086,
+		lng: -88.3504218,
 		iconColor: "#6C3483"
 	}
-	]
+];
+
+
+var Point = function(data) {
+
+	var self = this;
+	this.title = ko.observable(data.title);
+	this.lat = ko.observable(data.lat);
+	this.long = ko.observable(data.long);
+	this.iconColor = ko.observable(data.iconColor);
 };
+
+
+
+
+
+var ViewModel = function() {
+	var self = this;
+
+	this.pointList = ko.observableArray([]);
+
+	initialPoints.forEach(function(pointItem) {
+		self.pointList.push(new Point(pointItem));
+	});
+};
+
+
+
+ko.applyBindings(new ViewModel());
