cd10a5f5297717f18da590f7b7ce477f3035075b
Carter Carlson <carterbud1@gmail.com>
2017-12-27 17:42:38 -0600

    Figured out hover.  Almost too easy...

--- Project 5- Neighborhood Map/js/app.js
+++ Project 5- Neighborhood Map/js/app.js
@@ -3,31 +3,36 @@
 			title: "Raging Waves Waterpark",
 			location: {lat: 41.6967971, lng: -88.4176992},
 			iconColor: "1F618D",
-			url: "https://api.yelp.com/v3/businesses/raging-waves-waterpark-yorkville"
+			website: "https://api.yelp.com/v3/businesses/raging-waves-waterpark-yorkville",
+			image: ""
 		},
 		{
 			title: "Chicago Premium Outlets",
 			location: {lat: 41.7829187, lng: -88.3169258},
 			iconColor: "1E8449",
-			url: "https://api.yelp.com/v3/businesses/chicago-premium-outlets-aurora"
+			website: "https://api.yelp.com/v3/businesses/chicago-premium-outlets-aurora",
+			image: ""
 		},
 		{
 			title: "Chipotle",
 			location: {lat: 41.7546809, lng: -88.3089367},
 			iconColor: "BA4A00",
-			url: "https://api.yelp.com/v3/businesses/chipotle-mexican-grill-aurora-17?osq=chipotle"
+			website: "https://api.yelp.com/v3/businesses/chipotle-mexican-grill-aurora-17?osq=chipotle",
+			image: ""
 		},
 		{
 			title: "Funway",
 			location: {lat: 41.836494, lng: -88.2961499},
 			iconColor: "A93226",
-			website: "https://api.yelp.com/v3/businesses/funway-ultimate-entertainment-center-batavia"
+			website: "https://api.yelp.com/v3/businesses/funway-ultimate-entertainment-center-batavia",
+			image: ""
 		},
 		{
 			title: "Phillips Park Zoo",
 			location: {lat: 41.7494086, lng: -88.3504218},
 			iconColor: "6C3483",
-			url: "https://api.yelp.com/v3/businesses/phillips-park-zoo-aurora"
+			website: "https://api.yelp.com/v3/businesses/phillips-park-zoo-aurora",
+			image: ""
 		}
 		];
 
@@ -45,7 +50,7 @@
 
 		//var iconHighlighted = makeMarkerIcon('F2F3F4');
 		//var iconDefault = makeMarkerIcon('0091ff');
-		var largeInfoWindow = new google.maps.InfoWindow();
+		largeInfoWindow = new google.maps.InfoWindow();
 		var iconHighlighted = makeMarkerIcon('F2F3F4');
 
 
@@ -53,40 +58,54 @@
 			var position = initialPoints[i].location;
 			var title = initialPoints[i].title;
 			var iconColor = initialPoints[i].iconColor;
+			var yelp_url = initialPoints[i].url;
 			var iconDefault = makeMarkerIcon('1F818D');
 
 
 
 // https://discussions.udacity.com/t/yelp-v3-implementation/235928/18
 /// Yelp oauth
+/*
+			var bearerToken;
       var cors_anywhere_url = 'https://cors-anywhere.herokuapp.com/';
       var yelp_auth_url = cors_anywhere_url + "https://api.yelp.com/oauth2/token";
       $.ajax({
           url: yelp_auth_url,
-          method: "POST",
+          type: "POST",
           data: {
-              grant_type: 'client_credentials',
+              client_secret: 'Y3RWO7eFpu5bJYRarOFvTAm86Qez8qB8LdWPG9dia9oJlIJGqDCtnr6SQEJgqHOM',
               client_id: '2VGbrnpR6An-WQ6o55yRdQ',
-              client_secret: 'Y3RWO7eFpu5bJYRarOFvTAm86Qez8qB8LdWPG9dia9oJlIJGqDCtnr6SQEJgqHOM'
+              grant_type: 'client_credentials'
           },
       }).done(function(response){
-          console.log(response); // the response has the access_token
-          // initialPoints[i].url = response.url;
-          // initialPoints[i].image_url = response.image_url;
+          // initialPoints[i].website = response.url;
+          // initialPoints[i].image = response.image_url;
           // initialPoints[i].rating = response.rating;
-
-
+          bearerToken = response.access_token;
+          //console.log(response);
+          //console.log(bearerToken);
       }).fail(function(error){
           console.log("An error occured in getting Yelp access token!");
       });
 
 
+      $.ajax({
+      	url: yelp_auth_url,
+      	type: "GET",
+	      data: {
+	            client_secret: 'Y3RWO7eFpu5bJYRarOFvTAm86Qez8qB8LdWPG9dia9oJlIJGqDCtnr6SQEJgqHOM',
+	            client_id: '2VGbrnpR6An-WQ6o55yRdQ',
+	            grant_type: 'client_credentials'
+	        },
+    		headers: {"Authorization" : "Bearer " + bearerToken},
+      	success: function(response) {
+      		console.log(response);}
+      });
 
-
-
+*/
 
 			// create a marker per location and put it in an array
-			var marker = new google.maps.Marker({
+			marker = new google.maps.Marker({
 				map: map,
 				position: position,
 				title: title,
@@ -163,6 +182,7 @@
 		document.getElementById('hide-locations').addEventListener('click', hideLocations);
 		document.getElementById('show-sidebar').addEventListener('click', showSidebar);
 
+
 	  // Tale in a color and create a marker icon of the color
 	  function makeMarkerIcon(markerColor) {
 	  var markerImage = new google.maps.MarkerImage(
@@ -174,32 +194,6 @@
 	    new google.maps.Size(21,34));
 	  return markerImage;
 		}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 var Point = function(data) {
@@ -218,11 +212,12 @@ var ViewModel = function() {
 	initialPoints.forEach(function(pointItem) {
 		self.pointList.push(new Point(pointItem));
 	});
-/*	this.currentPoint = ko.observable(this.pointList()[0]); */
 
-	this.setPoint = function(clickedPoint) {
-		self.currentPoint(clickedPoint);
-	};
+	this.currentPoint = ko.observable(this.pointList()[0]);
+
+	this.setPoint = function() {
+		populateInfoWindow
+	}
 };
 
 ko.applyBindings(new ViewModel());
